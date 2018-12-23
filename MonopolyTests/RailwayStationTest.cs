39d536fcf0c315ea678aa87ff304f8be2c3f46b0
unknown <atl11384@gmail.com>
2018-12-23 12:55:32 +0000

    Tests added for railway station class

--- MonopolyTests/RailwayStationTest.cs
+++ MonopolyTests/RailwayStationTest.cs
@@ -1,14 +1,48 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Monopoly;
 
 namespace MonopolyTests
 {
     [TestClass]
-    public class UnitTest1
+    public class RailwayStationTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        RailwayStation station1;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            station1 = new RailwayStation("Fenchurch Street", 200, 50, 50, 200);
+        }
+
+        [TestMethod]
+        public void Can_Get_Name()
+        {
+            Assert.AreEqual("Fenchurch Street", station1.Name);
+        }
+
+        [TestMethod]
+        public void Can_Get_Price()
+        {
+            Assert.AreEqual(200, station1.Price);
+        }
+
+        [TestMethod]
+        public void Can_Get_ReMortgageValue()
+        {
+            Assert.AreEqual(50, station1.ReMortgageValue);
+        }
+
+        [TestMethod]
+        public void Can_Get_RenIfAlone()
+        {
+            Assert.AreEqual(50, station1.RentIfAlone);
+        }
+
+        [TestMethod]
+        public void Can_Get_RentIfSet()
+        {
+            Assert.AreEqual(200, station1.RentIfSet);
         }
     }
 }
