7241af8ebfc2e9f98eb6ebb26bd35bde23527802
unknown <atl11384@gmail.com>
2018-12-24 15:06:04 +0000

    Middlcard class added and test class created. All tests passing

---
+++ Monopoly/MiddleCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly
+{
+    public class MiddleCard
+    {
+        public string Name { get; set; }
+        public string Instruction { get; set; }
+
+        public MiddleCard(string Name, string Instruction)
+        {
+            this.Name = Name;
+            this.Instruction = Instruction;
+        }
+    }
+}
---
+++ MonopolyTests/MiddleCardTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Monopoly;
+
+namespace MonopolyTests
+{
+    [TestClass]
+    public class MiddleCardTest
+    {
+        MiddleCard middleCard1;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            middleCard1 = new MiddleCard("Chance", "Go to jail. Go directly to jail, do not pass go, do not collect £200");
+        }
+
+        [TestMethod]
+        public void Can_Get_Name()
+        {
+            Assert.AreEqual("Chance", middleCard1.Name);
+        }
+
+        [TestMethod]
+        public void Can_Get_Instruction()
+        {
+            Assert.AreEqual("Go to jail. Go directly to jail, do not pass go, do not collect £200", middleCard1.Instruction);
+        }
+    }
+}
