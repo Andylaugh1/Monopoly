bcbbc899308e95522cf8264fd20a7fd75ccc28ac
unknown <atl11384@gmail.com>
2018-12-25 13:41:17 +0000

    Dice class added and along with unit tests

---
+++ Monopoly/Dice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly
+{
+    public class Dice
+    {
+        public Random Random { get; set; }
+        public int SidesCount { get; set; }
+
+        public Dice()
+        {
+            this.SidesCount = 6;
+            this.Random = new Random();
+
+        }
+
+        public int Roll()
+        {
+            return this.Random.Next(1, this.SidesCount + 1);
+        }
+    }
+}
---
+++ MonopolyTests/DiceTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Monopoly;
+
+namespace MonopolyTests
+{
+    [TestClass]
+    public class DiceTest
+    {
+        Dice dice1;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            dice1 = new Dice();
+        }
+
+        [TestMethod]
+        public void Can_Get_Number_Of_Sides()
+        {
+            Assert.AreEqual(6, dice1.SidesCount);
+        }
+        
+        [TestMethod]
+        public void Can_Roll_Dice_For_Random_Number()
+        {
+            Assert.AreEqual(4, dice1.Roll());
+        }
+    }
+}
